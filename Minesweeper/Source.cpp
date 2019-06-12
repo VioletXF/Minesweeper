9561e69ed51521f07b8b952193f2ef5aac4613c1
mir99712 <mir99712@naver.com>
2019-06-13 01:04:58 +0900

    a

--- Minesweeper/Source.cpp
+++ Minesweeper/Source.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 struct gameplate {
 	int xsize;
 	int ysize;
 	int mineNum;
+	vector<vector<int>> mapData;
 };
+void output(vector<vector<int>> mapData);
 int main() {
+	//Set Game
 	gameplate plate;
 	cout << "판의 가로 길이:";
 	cin >> plate.xsize;
 	cout << "판의 세로 길이:";
 	cin >> plate.ysize;
+	plate.mapData = vector<vector<int>>(plate.ysize, vector<int>(plate.xsize,0));
+
 	cout << "지뢰의 개수 (1 미만의 값 입력 시 자동):";
 	cin >> plate.mineNum;
 	if (plate.mineNum < 1) {
 		plate.mineNum = plate.xsize * plate.ysize / 2;
+	}
+
+
+	output(plate.mapData);
+}
+
+void output(vector<vector<int>> mapData) {
+	for (int i = 0; i < mapData.size(); i++) {
+		for (int j = 0; j < mapData[i].size(); j++) {
+			cout << mapData[i][j];
+		}
+		cout<<endl;
 	}
 }
